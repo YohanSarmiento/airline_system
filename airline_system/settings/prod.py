645892b355ad1801172b2e42ed73eca06417e105
YohanSarmiento <yohan.sarmiento23@gmail.com>
2024-04-23 10:31:34 -0500

    settings for deployment

--- airline_system/settings/prod.py
+++ airline_system/settings/prod.py
@@ -1,8 +1,13 @@
 import os
 from .common import *
+import dj_database_url
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['airline-prod-eb3757937c68.herokuapp.com']
+
+DATABASES = {
+    "default": dj_database_url.config()
+}
